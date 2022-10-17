18d18645b796da9702775c3be6d1d3a2148372fe
Niklas <niklas_edelstam@hotmail.com>
2022-10-17 16:37:52 +0200

    petits changements

--- app/screens/LoginScreen.tsx
+++ app/screens/LoginScreen.tsx
@@ -1,19 +1,12 @@
-
-import React, {useContext, useState} from 'react';
-import { StatusBar } from 'expo-status-bar';
+import React, { useContext, useState } from "react";
+import { StatusBar } from "expo-status-bar";
 import { IUser } from "../interfaces/IUser";
 import { AxiosFunction } from "../api/AxiosFunction";
 import { AxiosError, AxiosResponse } from "axios";
 import useAuth from "../hooks/useAuth";
 import BackgroundImg from "../components/BackgroundImg";
 import colors from "../config/colors";
-import {
-  Button,
-  Text,
-  TextInput,
-  View,
-  StyleSheet,
-} from 'react-native';
+import { Button, Text, TextInput, View, StyleSheet } from "react-native";
 
 /*
 const initialValues = {
@@ -24,65 +17,64 @@ const initialValues = {
 const LOGIN_URL = "auth/login";
 
 const LoginScreen = () => {
-    //const [userInfo, setUserInfos] = useState<IUser>(initialValues);
-    // const { setAuth, persist, setPersist } = useAuth();
-    const { setAuth } = useAuth();
-    const [email, setEmail] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
+  //const [userInfo, setUserInfos] = useState<IUser>(initialValues);
+  // const { setAuth, persist, setPersist } = useAuth();
+  const { setAuth } = useAuth();
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
-    const { postQuery } = AxiosFunction();
+  const { postQuery } = AxiosFunction();
 
-    const login = (email: string | undefined, password: string | undefined) => {
-        const postData = { email, password};
-        console.log(postData);
-        postQuery(LOGIN_URL, postData).then((response: AxiosResponse) => {
-            const accessToken = response.data.accessToken;
-            const role = response.data.idRole;
-            console.log(response.data);
-            //setUserInfos(response.data);
-            setAuth?.({ role, accessToken });
-        }).catch((error: AxiosError) => {
-            console.log(error);
-        })
-    };
+  const login = (email: string | undefined, password: string | undefined) => {
+    const postData = { email, password };
+    console.log(postData);
+    postQuery(LOGIN_URL, postData)
+      .then((response: AxiosResponse) => {
+        const accessToken = response.data.accessToken;
+        const role = response.data.idRole;
+        console.log(response.data);
+        //setUserInfos(response.data);
+        setAuth?.({ role, accessToken });
+      })
+      .catch((error: AxiosError) => {
+        console.log(error);
+      });
+  };
 
-    return (
-        <View style={styles.container}>
-          <View style={styles.wrapper}>
-          <Text style={styles.text}>Adresse mail</Text>
-            <TextInput
-              style={styles.input}
-              value={email}
+  return (
+    <View style={styles.container}>
+      <View style={styles.wrapper}>
+        <Text style={styles.text}>Adresse mail</Text>
+        <TextInput
+          style={styles.input}
+          value={email}
           placeholder="Enter email"
-          onChangeText={text => setEmail(text)}
-            />
-    
-            <Text style={styles.text}>Mot de passe</Text>
-            <TextInput
-              style={styles.input}
-              value={password}
+          onChangeText={(text) => setEmail(text)}
+        />
+
+        <Text style={styles.text}>Mot de passe</Text>
+        <TextInput
+          style={styles.input}
+          value={password}
           placeholder="Enter password"
-          onChangeText={text => setPassword(text)}
-              secureTextEntry
-            />
-          
-          </View>
-            <View style={styles.button}>
-            <Button
-              title="Se connecter"
-              color={colors.primary}
-              onPress={() => {
-                login(email, password);
-              }}
-            />
-             </View>
-    
-            <View style={{flexDirection: 'row', marginTop: 20}}>
-              <Text>Inscription </Text>
-            </View>
-         
+          onChangeText={(text) => setPassword(text)}
+          secureTextEntry
+        />
       </View>
-    </>
+      <View style={styles.button}>
+        <Button
+          title="Se connecter"
+          color={colors.primary}
+          onPress={() => {
+            login(email, password);
+          }}
+        />
+      </View>
+
+      <View style={{ flexDirection: "row", marginTop: 20 }}>
+        <Text>Inscription </Text>
+      </View>
+    </View>
   );
 };
 
@@ -104,7 +96,7 @@ const styles = StyleSheet.create({
     borderColor: "#bbb",
     borderRadius: 5,
     paddingHorizontal: 14,
-    paddingVertical: 4
+    paddingVertical: 4,
   },
   button: {
     position: "absolute",
