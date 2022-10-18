967f74fc2ec3a06139c68b45baa9e8682a059bd7
nanaplouf <vera78s@gmail.com>
2022-10-18 14:24:18 +0200

    page home et bar de navigation pour home

--- app/navigation/RootNavigator.tsx
+++ app/navigation/RootNavigator.tsx
@@ -1,4 +1,5 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Button } from "react-native";
 import BookingScreen from "../screens/BookingScreen";
 import HomeScreen from "../screens/HomeScreen";
 import LoginScreen from "../screens/LoginScreen";
@@ -17,10 +18,34 @@ export const RootNavigator = () => {
     return (
         <Stack.Navigator>
             <Stack.Group>
-                <Stack.Screen name="Home" component={HomeScreen} />
-                <Stack.Screen name="Login" component={LoginScreen} />
-                <Stack.Screen name="Register" component={RegisterScreen} />
-                <Stack.Screen name="Booking" component={BookingScreen} />
+                <Stack.Screen
+                    name="Home"
+                    component={HomeScreen}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                    name="Login"
+                    component={LoginScreen}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                    name="Register"
+                    component={RegisterScreen}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                    name="Booking"
+                    component={BookingScreen}
+                    options={{
+                        headerRight: () => (
+                            <Button
+                                onPress={() => alert('This is a button!')}
+                                title="login"
+                                color="#fff"
+                            />
+                        ),
+                    }}
+                />
             </Stack.Group>
         </Stack.Navigator>
     );
