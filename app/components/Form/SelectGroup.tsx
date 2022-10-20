c19a73d4bdec085b4c3ee0aa78656ff72c04f8d7
ClementG60 <cle_guillaume@hotmail.fr>
2022-10-20 14:44:28 +0200

    modification form login

--- app/components/Form/SelectGroup.tsx
+++ app/components/Form/SelectGroup.tsx
@@ -3,9 +3,15 @@ import React from "react";
 import colors from "../../config/colors";
 import { Picker } from "@react-native-picker/picker";
 
+interface IData {
+  id: number;
+  name: string;
+}
+
 type SelectGroupProps = {
   label?: string;
   value: string;
+  data?: Array<IData>;
   onValueChange: (value: string) => void;
   error?: boolean;
   errorDetails?: string;
@@ -14,6 +20,7 @@ type SelectGroupProps = {
 const SelectGroup = ({
   label,
   value,
+  data,
   onValueChange,
   error = false,
   errorDetails,
@@ -21,9 +28,27 @@ const SelectGroup = ({
   return (
     <View>
       {label && <Text style={styles.label}>{label}</Text>}
-      <Picker selectedValue={value} onValueChange={onValueChange}>
-        <Picker.Item label="Java" value="java" />
-        <Picker.Item label="JavaScript" value="js" />
+      <Picker
+        selectedValue={value}
+        onValueChange={onValueChange}
+        style={[styles.select, styles.selectBorder]}
+      >
+        <Picker.Item
+          label="Choisir la catégorie"
+          style={styles.placeholder}
+          enabled={false}
+          value=""
+        />
+        {data &&
+          data.map((service) => {
+            return (
+              <Picker.Item
+                label={service.name}
+                style={styles.item}
+                value={service.id}
+              />
+            );
+          })}
       </Picker>
       {errorDetails && <Text style={styles.errorText}>{errorDetails}</Text>}
     </View>
@@ -36,24 +61,29 @@ const styles = StyleSheet.create({
   label: {
     color: colors.text,
     marginBottom: 2,
+    fontWeight: "bold",
   },
-  inputBorder: {
+  selectBorder: {
     borderColor: colors.grey,
   },
-  inputBorderError: {
+  selectBorderError: {
     borderColor: colors.red,
   },
-  input: {
-    borderWidth: 1,
-    borderRadius: 5,
-    marginBottom: 6,
-    paddingLeft: 5,
-    backgroundColor: colors.white,
+  select: {
+    marginBottom: 20
   },
   errorText: {
     color: colors.red,
     fontWeight: "bold",
     marginBottom: 6,
     with: "auto",
+  },
+  placeholder: {
+    fontSize: 14,
+    paddingVertical: 2,
+    color: "#929294",
+  },
+  item: {
+    fontSize: 14,
   },
 });
