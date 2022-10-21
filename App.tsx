8c861e593d3cdc91b1176cbb4d48041065e22680
ClementG60 <cle_guillaume@hotmail.fr>
2022-10-21 13:47:36 +0200

    ajout bottom sheet modal

--- App.tsx
+++ App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { RootNavigator } from "./app/navigation/RootNavigator";
 import { AuthProvider } from "./app/context/AuthProvider";
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function App() {
   return (
     <AuthProvider>
       <NavigationContainer>
-        <RootNavigator />
+          <RootNavigator />
       </NavigationContainer>
     </AuthProvider>
   );
